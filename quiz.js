c419ca66564593edaa3ecf15773141b459abedad
oOFaYOo <nika.tseleva@mail.ru>
2020-10-20 20:50:42 +0500

    Final code fix

--- quiz.js
+++ quiz.js
@@ -1,74 +1,54 @@
-let question = document.getElementById("question");
-let numberOfQuestion = document.getElementById("numberOfQuestion");
+let data = [
+        {question:"Who was the first President of the United States?", number:"question 1 of 5", answer1:"George Washington", answer2:"Thomas Jefferson", answer3:"Thomas Edison", answer4:"I don't know", rightAnswer:"George Washington"},
+        {question:"What is the answer to the Ultimate Question of Life, the Universe, and Everything?", number: "question 2 of 5", answer1:"Pi", answer2:"42", answer3:"Wah?", answer4:"I don't know", rightAnswer:"42"},
+        {question:"Do you love to code?", number: "question 3 of 5", answer1:"No", answer2:"Yes", answer3:"Hell Yeah", answer4:"Hell No", rightAnswer:"Hell Yeah"},
+        {question:"What's the best programming language?", number:"question 4 of 5", answer1:"Python", answer2:"C#", answer3:"Php", answer4:"Javascript", rightAnswer:"Javascript"},
+        {question:"Is Leonardo DiCaprio Awesome?", number:"question 5 of 5", answer1:"Yes", answer2:"No", answer3:"Maybe", answer4:"He's okay", rightAnswer:"Yes"}
+];
 let buttons = document.getElementsByClassName("buttons");
-let score = document.getElementById("score");
-let answer1 = document.getElementById("answer1");
-let answer2 = document.getElementById("answer2");
-let answer3 = document.getElementById("answer3");
-let answer4 = document.getElementById("answer4");
-let scoreResult = 0;
 let button1 = document.getElementById("button1");
 let button2 = document.getElementById("button2");
 let button3 = document.getElementById("button3");
 let button4 = document.getElementById("button4");
+let scoreResult = 0;
+let i = 0;
+
+drawNewQuestion(data[i]);
 
 for(let button of buttons){
-    button.onclick = updateQuestionNumberAnswers;
+    button.onclick = actionForClick;
 }
 
-function updateQuestionNumberAnswers (event) {
-    if (question.innerHTML === "Who was the first President of the United States?"){
-        writeTextInQuestionNumberAnswers("What is the answer to the Ultimate Question of Life, the Universe, and Everything?", "question 2 of 5", "Pi", "42", "Wah?", "I don't know");
-        if(event.path[0] === button1){
-            scoring();
-        }
-        return;
-    }
-    if (question.innerHTML === "What is the answer to the Ultimate Question of Life, the Universe, and Everything?"){
-        writeTextInQuestionNumberAnswers("Do you love to code?", "question 3 of 5", "No", "Yes", "Hell Yeah", "Hell No");
-        if(event.path[0] === button2){
-            scoring();
-        }
-        return;
-    }
-    if (question.innerHTML === "Do you love to code?"){
-        writeTextInQuestionNumberAnswers("What's the best programming language?", "question 4 of 5", "Python", "C#", "Php", "Javascript");
-        if(event.path[0] === button3){
-            scoring();
-        }
-        return;
-    }
-    if (question.innerHTML === "What's the best programming language?"){
-        writeTextInQuestionNumberAnswers("Is Leonardo DiCaprio Awesome?", "question 5 of 5", "Yes", "No", "Maybe", "He's okay");
-        if(event.path[0] === button4){
-            scoring();
-        }
-        return;
-    }
-    if (question.innerHTML === "Is Leonardo DiCaprio Awesome?"){
-        if(event.path[0] === button1){
-            scoring();
-        }
-        showResult();
+function checkAnswer(questionAnswerInfo, event){
+    if (questionAnswerInfo.rightAnswer === event.path[0].answer){
+        scoreResult = scoreResult + 1;
     }
 }
 
-function writeTextInQuestionNumberAnswers(textOfQuestion, textOfNumberOfQuestion, textOfAnswer1, textOfAnswer2, textOfAnswer3, textOfAnswer4) {
-    question.innerHTML = textOfQuestion;
-    numberOfQuestion.innerHTML = textOfNumberOfQuestion;
-    answer1.innerHTML = textOfAnswer1;
-    answer2.innerHTML = textOfAnswer2;
-    answer3.innerHTML = textOfAnswer3;
-    answer4.innerHTML = textOfAnswer4;
+function actionForClick(event) {
+    checkAnswer(data[i], event);
+    i++;
+    if (i > data.length-1){
+        showResult()
+    }
+    else drawNewQuestion(data[i]);
+}
+
+function drawNewQuestion(questionObject) {
+        document.getElementById("question").innerHTML = questionObject.question;
+        document.getElementById("numberOfQuestion").innerHTML = questionObject.number;
+        document.getElementById("answer1").innerHTML = questionObject.answer1;
+        document.getElementById("answer2").innerHTML = questionObject.answer2;
+        document.getElementById("answer3").innerHTML = questionObject.answer3;
+        document.getElementById("answer4").innerHTML = questionObject.answer4;
+        button1.answer = questionObject.answer1;
+        button2.answer = questionObject.answer2;
+        button3.answer = questionObject.answer3;
+        button4.answer = questionObject.answer4;
 }
 
 function showResult() {
-    score.innerHTML = score.innerHTML + ` ${scoreResult}`;
-    score.hidden = false;
+    document.getElementById("score").innerHTML = document.getElementById("score").innerHTML + ` ${scoreResult}`;
+    document.getElementById("score").hidden = false;
     document.querySelectorAll("#question, .answersWithButtons, #numberOfQuestion").forEach((element) => element.hidden = true);
 }
-
-function scoring() {
-    scoreResult = scoreResult + 1;
-}
-
